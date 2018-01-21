a061a85cea601af8dfba906d6cf1800ed1fa94c7
emily <emilyhannan@D.local>
2018-01-20 16:51:18 -0800

    created more game logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardOne = cards[0];
+cardsInPlay.push('queen');
+console.log("User flipped queen");
 
+var cardTwo = cards[3];
+cardsInPlay.push('king');
+console.log("user flipped king");
+
+var cardsInPlay = [];
+
+var checkForMatch = function (){
+    if (cardsInPlay[0] === cardsInPlay[1])
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
+}
+
+var flipCard = function(cardId) {
+    console.log("User flipped " + cardOne);
+    console.log("User flipped" + cards[cardId]);
+}
